207dea3f5378b492bf70dc653307db282fa55694
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-08-10 21:08:56 +0200

    Update Finance.py

--- Finance.py
+++ Finance.py
@@ -3,10 +3,17 @@ import yfinance as yf
 import streamlit as st
 msft = yf.Ticker("CFR.JO")
 info = msft.info
-st.write('Trailing PE:',info['trailingPE'])
-st.write('ROE:',info['returnOnEquity'])
+ROE = info['returnOnEquity']
+PE = info['trailingPE']
+K = info['payoutRatio']
+RR = 1-K
+g = ROE*RR
+st.write('Trailing PE:',PE)
+st.write('ROE:',ROE)
+st.write('Payout ratio:',K)
+st.write('Retention Rate:',RR)
+st.write('Growth Rate:',g)
 st.write('Ticker:',info['symbol'])
 st.write('Name:',info['shortName'])
-st.write('Payout ratio:',info['payoutRatio'])
 st.write(info)
 #st.write(info.keys())
