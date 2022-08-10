b0372cbe218052ed7442d7f1d45c44e346195888
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-08-10 20:54:57 +0200

    Update Finance.py

--- Finance.py
+++ Finance.py
@@ -2,6 +2,6 @@ import yfinance as yf
 import streamlit as st
 msft = yf.Ticker("CFR.JO")
 info = msft.info
-#st.write(info)
-st.write(info['trailingPE'])
-st.write(info.keys())
+st.write('Trailing PE:',info['trailingPE'])
+st.write(info)
+#st.write(info.keys())
