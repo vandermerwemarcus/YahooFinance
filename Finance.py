a91bf2a05bc5a41838b1ab645467b5bc1ea05c86
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-08-10 21:05:24 +0200

    Update Finance.py

--- Finance.py
+++ Finance.py
@@ -6,5 +6,7 @@ info = msft.info
 st.write('Trailing PE:',info['trailingPE'])
 st.write('ROE:',info['returnOnEquity'])
 st.write('Ticker:',info['symbol'])
+st.write('Name:',info['shortName'])
+st.write('Payout ratio:',info['payoutRatio'])
 st.write(info)
 #st.write(info.keys())
