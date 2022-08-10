00c6d7658af23dcf609d9dad4a5fe391649ce851
vandermerwemarcus <57601387+vandermerwemarcus@users.noreply.github.com>
2022-08-10 21:00:38 +0200

    Update Finance.py

--- Finance.py
+++ Finance.py
@@ -1,9 +1,10 @@
+#https://analyzingalpha.com/yfinance-python
 import yfinance as yf
 import streamlit as st
 msft = yf.Ticker("CFR.JO")
 info = msft.info
 st.write('Trailing PE:',info['trailingPE'])
 st.write('ROE:',info['returnOnEquity'])
-
+st.write('ROE:',info['symbol'])
 st.write(info)
 #st.write(info.keys())
